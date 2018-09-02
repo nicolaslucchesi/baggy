01c30f1bf9ac8db6dbdd951bbfa83a1ee625bdc7
Manuel Franco <39923794+Manueelfranco@users.noreply.github.com>
2018-09-02 14:41:57 -0300

    Entry sin subrayado solucionado

--- MobileExample/MobileExample.Android/CustomEntryRenderer.cs
+++ MobileExample/MobileExample.Android/CustomEntryRenderer.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using MobileExample;
+﻿using MobileExample.CustomElements;
 using MobileExample.Droid;
-using MobileExample.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
--- MobileExample/MobileExample/CustomElements/CustomEntry.cs
+++ MobileExample/MobileExample/CustomElements/CustomEntry.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
 
-namespace MobileExample.Views
+namespace MobileExample.CustomElements
 {
     public class CustomEntry : Entry
     {
