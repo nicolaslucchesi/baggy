2f96bd322cf59c214c4cf2616869ecd97e4c4850
Matias Vega <matiasfvega95@gmail.com>
2018-09-03 23:11:22 -0300

    Ver mochila mejorado
    Se mejoró la interfaz de ver mochila

--- MobileExample/MobileExample/Views/Mochila/VerMochila.xaml.cs
+++ MobileExample/MobileExample/Views/Mochila/VerMochila.xaml.cs
@@ -13,11 +13,50 @@ namespace MobileExample.Views
 	public partial class VerMochila : ContentPage
 	{
         public MochilaViewModel mochilaViewModel { get; set; }
+        public int tapCount;
+
         public VerMochila(int Id)
         {
             InitializeComponent();
             mochilaViewModel = (MochilaViewModel)DatabaseHelper.db.Get<Mochila>(Id);
             BindingContext = this.mochilaViewModel = mochilaViewModel;
+
+            tapCount = 0;
+
+
+            var topLeft = new Image { Source = "Libro.png" };
+            var topRight = new Image { Source = "Editar1.png" };
+            var bottomLeft = new Image { Source = "mochila.png" };
+            var bottomRight = new Image { Source = "Libro.png" };
+
+            GridElementos.Children.Add(topLeft, 0, 0);
+            GridElementos.Children.Add(topRight, 1, 0);
+            GridElementos.Children.Add(bottomLeft, 0, 1);
+            GridElementos.Children.Add(bottomRight, 1, 1);
+        }
+
+        void OnTapGestureRecognizerTapped(object sender, EventArgs args)
+        {
+            tapCount++;
+            var imageSender = (Image)sender;
+            // watch the monkey go from color to black&white!
+            if (tapCount % 2 == 0)
+            {
+                imageSender.IsVisible = false;
+                Blurr.IsVisible = true;
+                GridElementos.IsVisible = true;
+            } 
+        }
+
+        void CerrarMochila(object sender, EventArgs args)
+        {
+            tapCount++;
+            if (tapCount % 2 == 0)
+            {
+                MochilaCerrada.IsVisible = true;
+                Blurr.IsVisible = false;
+                GridElementos.IsVisible = false;
+            }
         }
     }
 }
